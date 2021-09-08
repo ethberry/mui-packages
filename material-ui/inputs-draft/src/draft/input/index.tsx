41883b33ff60f89a9ea1a2266f7a6beefb760ab8
Karafizi Arthur <karafizi.artur@gmail.com>
2021-09-08 11:30:20 +0800

    feat: implement material-ui-inputs-draft

---
+++ material-ui/inputs-draft/src/draft/input/index.tsx
@@ -0,0 +1,20 @@
+// import { FC, Fragment, forwardRef, Ref, useCallback, useEffect, useImperativeHandle, useRef } from "react";
+import { forwardRef, Fragment } from "react";
+import { InputBaseComponentProps } from "@material-ui/core/InputBase";
+import { EditorState } from "draft-js";
+import { IRichTextEditorRef, RichTextEditor, TToolbarControl } from "@gemunion/mui-rte";
+
+export interface IRichTextInputProps extends Omit<InputBaseComponentProps, "value"> {
+  doFocus?: boolean;
+  onStateChange?: (state: EditorState) => void;
+  controls?: Array<TToolbarControl>;
+}
+
+export const RichTextInput = forwardRef<IRichTextEditorRef, IRichTextInputProps>((props, ref) => {
+  const { onStateChange, ...rest } = props;
+  return (
+    <Fragment>
+      <RichTextEditor onChange={onStateChange} {...rest} ref={ref} />
+    </Fragment>
+  );
+});
