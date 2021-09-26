1be1c0dc5e49b9003cc5dfed9b17b73ae9382e29
Yury Toporov <gemuniongroup@gmail.com>
2021-09-26 12:15:10 +0800

    refactor: renovate master only [skip ci]

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ set -e # this will cause the shell to exit immediately if any command exits with
 
 export NODE_ENV=test
 
-lerna bootstrap --hoist && lerna run lint --stream --parallel
+lerna bootstrap --hoist && lerna run lint --stream
 
 # lerna run lint --stream --parallel
 
