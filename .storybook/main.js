303d3ed8e3ae36c538bd8ae4ea0cd9880b205891
TrejGun <trejgun@gmail.com>
2021-11-19 09:05:40 +0800

    chore: rollback storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,53 @@
 module.exports = {
   stories: [
-    "../material-ui/**/*.stories.@(js|jsx|ts|tsx)",
+    "../material-ui/**/*.stories.@(ts|tsx)",
   ],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
   ],
+  webpackFinal: config => {
+    config.module.rules.push({
+      test: /\.[tj]sx?$/,
+      exclude: [/node_modules/],
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            babelrc: false,
+            presets: [
+              [
+                "@babel/env",
+                {
+                  modules: false,
+                  targets: {
+                    browsers: ["> 1%"],
+                  },
+                },
+              ],
+              [
+                "@babel/typescript",
+                {
+                  isTSX: true,
+                  allExtensions: true,
+                },
+              ],
+              [
+                "@babel/react",
+                {
+                  "runtime": "automatic",
+                },
+              ],
+            ],
+            plugins: [
+              "@babel/plugin-proposal-nullish-coalescing-operator",
+              "@babel/plugin-proposal-optional-chaining",
+              "babel-plugin-inline-import",
+            ],
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
