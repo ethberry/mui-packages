c3b7618f73e3723a5bb3ecdea4712a3b1f43eaf4
TrejGun <trejgun@gmail.com>
2021-12-28 11:53:48 +0800

    fix: login

--- other/common-pages/src/common/protected/index.tsx
+++ other/common-pages/src/common/protected/index.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment, useContext } from "react";
+import { FC, Fragment, useContext, useState, useEffect } from "react";
 
 import { UserContext } from "@gemunion/provider-user";
 
@@ -7,7 +7,17 @@ import { Login } from "../../guest/login";
 export const Protected: FC = props => {
   const { children } = props;
 
+  const [isReady, setIsReady] = useState(false);
   const user = useContext(UserContext);
+
+  useEffect(() => {
+    const id = setTimeout(() => setIsReady(true), 100);
+    return () => clearTimeout(id);
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
 
   if (!user.isAuthenticated()) {
     return <Login />;
