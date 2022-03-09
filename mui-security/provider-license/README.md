f93941ab69f50301a665dc58d5d1535c24f85fa7
TrejGun <trejgun@gmail.com>
2022-03-09 12:17:30 +0800

    chore: more provider license to common

--- mui-security/provider-license/README.md
+++
@@ -1,1 +0,0 @@
-# Gemunion license provider
--- mui-security/provider-license/src/context.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export interface ILicenseContext {
-  isValid: () => boolean;
-  refresh: () => Promise<void>;
-}
-
-export const ApiContext = createContext<ILicenseContext>(undefined!);
