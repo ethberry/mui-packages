3a6fd731d528bdd61f73de2ab17d9f1dc6b190a0
TrejGun <trejgun@gmail.com>
2022-03-09 19:18:24 +0800

    fix: roll back generics

--- providers/provider-localization/src/provider/provider.tsx
+++ providers/provider-localization/src/provider/provider.tsx
@@ -1,18 +1,20 @@
-import { FC } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 import { IntlProvider } from "react-intl";
 
 import { useSettings } from "@gemunion/provider-settings";
 
 import { flattenMessages } from "./utils";
 
-interface ILocalizationProviderProps {
-  i18n: Record<string, any>;
-  defaultLanguage: string;
+interface ILocalizationProviderProps<T extends string> {
+  i18n: Record<T, any>;
+  defaultLanguage: T;
 }
 
-export const LocalizationProvider: FC<ILocalizationProviderProps> = props => {
+export const LocalizationProvider = <T extends string>(
+  props: PropsWithChildren<ILocalizationProviderProps<T>>,
+): ReactElement | null => {
   const { children, i18n, defaultLanguage } = props;
-  const settings = useSettings();
+  const settings = useSettings<T>();
 
   return (
     <IntlProvider
