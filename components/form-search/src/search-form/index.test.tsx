0f30dd11c44bf64d0ef6964c031d9d65d24ea5f8
TrejGun <trejgun@gmail.com>
2022-04-22 10:08:45 +0800

    test: common search form

---
+++ components/form-search/src/search-form/index.test.tsx
@@ -0,0 +1,38 @@
+import { IntlProvider } from "react-intl";
+import { cleanup, render } from "@testing-library/react";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { MemoryRouter } from "react-router-dom";
+
+import { CommonSearchForm } from "./index";
+
+afterEach(cleanup);
+
+const i18n = {
+  "form.labels.query": "Query",
+  "form.placeholders.query": "Query",
+  "form.hints.prompt": "Prompt",
+};
+
+// https://github.com/jaredpalmer/formik/issues/1543
+describe("<CommonSearchForm />", () => {
+  it("renders component", () => {
+    const formikProps = {
+      onSubmit: jest.fn(),
+      initialValues: {
+        number: 50,
+      },
+    };
+
+    const { asFragment } = render(
+      <MemoryRouter>
+        <ThemeProvider theme={createTheme()}>
+          <IntlProvider locale="en" messages={i18n}>
+            <CommonSearchForm {...formikProps} />
+          </IntlProvider>
+        </ThemeProvider>
+      </MemoryRouter>,
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
