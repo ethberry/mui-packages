98ed0f6f0efc01c13420c9052a35cdaac2b6f384
TrejGun <trejgun@gmail.com>
2022-05-01 19:37:43 +0800

    feat: make breadcrumbs better

--- components/breadcrumbs/src/index.tsx
+++ components/breadcrumbs/src/index.tsx
@@ -1,27 +1,34 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 import { FormattedMessage } from "react-intl";
 import { Breadcrumbs as MuiBreadcrumbs, Link, Typography } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 export interface IBreadcrumbsProps {
   path: Array<string>;
+  isHidden: boolean;
+  values: Array<Record<string, ReactNode>>;
 }
 
 export const Breadcrumbs: FC<IBreadcrumbsProps> = props => {
-  const { path } = props;
+  const { path, isHidden, values = [] } = props;
+
+  if (isHidden) {
+    return null;
+  }
+
   return (
     <MuiBreadcrumbs aria-label="breadcrumbs">
       {path.map((e, i) => {
         if (i === path.length - 1) {
           return (
             <Typography color="textPrimary" key={i}>
-              <FormattedMessage id={`pages.${e}.title`} />
+              <FormattedMessage id={`pages.${e}.title`} values={values[i]} />
             </Typography>
           );
         }
         return (
           <Link color="inherit" component={RouterLink} to={`/${e}`} key={i}>
-            <FormattedMessage id={`pages.${e}.title`} />
+            <FormattedMessage id={`pages.${e}.title`} values={values[i]} />
           </Link>
         );
       })}
