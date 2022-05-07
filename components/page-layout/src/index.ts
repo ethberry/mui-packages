0bce8c806233602acf9c2b6c87fb7c9b626d1892
TrejGun <trejgun@gmail.com>
2022-05-08 05:59:33 +0800

    feat: scroll to top

--- components/page-layout/src/index.ts
+++ components/page-layout/src/index.ts
@@ -2,4 +2,5 @@ export * from "./breadcrumbs";
 export * from "./button-toolbar";
 export * from "./page-header";
 export * from "./progress-overlay";
+export * from "./scroll-to-top";
 export * from "./spinner";
