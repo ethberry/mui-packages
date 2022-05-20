31a7dce2f8b80450775a5b396ffe8ec0e001c69e
Eugene Mormul <53538417+TbIKoBKa@users.noreply.github.com>
2022-05-20 12:52:33 +0300

    feat: open connect dialog useMetamask

--- other/hooks/src/useMetamask.tsx
+++ other/hooks/src/useMetamask.tsx
@@ -2,15 +2,18 @@ import { useSnackbar } from "notistack";
 import { useIntl } from "react-intl";
 import { useWeb3React } from "@web3-react/core";
 
+import { useWallet } from "@gemunion/provider-wallet";
+
 export const useMetamask = (fn: (...args: Array<any>) => Promise<unknown>) => {
   const { account } = useWeb3React();
 
   const { enqueueSnackbar } = useSnackbar();
   const { formatMessage } = useIntl();
+  const { setWalletConnectDialogOpen } = useWallet();
 
   return async (...args: Array<any>) => {
     if (!account) {
-      // TODO rework to use provider-popup and provider-wallet
+      setWalletConnectDialogOpen(true);
       enqueueSnackbar(formatMessage({ id: "snackbar.walletIsNotConnected" }), { variant: "error" });
       return;
     }
