a1467a3c7e70eacd3e8c5819ede87ce1152fb5fe
Karafizi Arthur <karafizi.artur@gmail.com>
2022-06-09 17:24:31 +0800

    fix: implement TextInput into MaskedInput

--- components/inputs-mask/src/mask/index.tsx
+++ components/inputs-mask/src/mask/index.tsx
@@ -1,10 +1,7 @@
 import { FC } from "react";
-import { Controller, useFormContext } from "react-hook-form";
-import { useIntl } from "react-intl";
-import { TextField } from "@mui/material";
+import { TextInput } from "@gemunion/mui-inputs-core";
 
 import { MaskedInputWrapper } from "./wrapper";
-import { useStyles } from "./styled";
 
 export interface IMaskedInputProps {
   name: string;
@@ -16,7 +13,6 @@ export interface IMaskedInputProps {
   allowLeadingZeros?: boolean;
   readOnly?: boolean;
   formatValue?: (value: string) => string | number;
-  InputLabelProps?: any;
   InputProps?: any;
   defaultValue?: any;
   value?: any;
@@ -24,78 +20,33 @@ export interface IMaskedInputProps {
   type?: "text" | "tel" | "password";
   format?: string;
   mask?: string;
-  placeholder?: string;
-  variant?: "standard" | "filled" | "outlined";
   onChange?: (event: { target: { name: string; value: string } }) => void;
-  label?: string;
 }
 
 export const MaskedInput: FC<IMaskedInputProps> = props => {
-  const {
-    name,
-    formatValue,
-    value: _value,
-    label,
-    InputLabelProps,
-    InputProps,
-    placeholder,
-    variant = "standard",
-    readOnly,
-    ...rest
-  } = props;
+  const { name, formatValue, value: _value, InputProps, readOnly, ...rest } = props;
 
-  const classes = useStyles();
-
-  const form = useFormContext<any>();
-  const error = form.formState.errors[name];
-  const touched = Boolean(form.formState.touchedFields[name]);
-
-  const suffix = name.split(".").pop() as string;
-  const { formatMessage } = useIntl();
-  const localizedLabel = label === void 0 ? formatMessage({ id: `form.labels.${suffix}` }) : label;
-  const localizedPlaceholder =
-    placeholder === void 0 ? formatMessage({ id: `form.placeholders.${suffix}` }) : placeholder;
-  const localizedHelperText = error && touched ? formatMessage({ id: error.message }, { label: localizedLabel }) : "";
+  const onValueChange = (field: any) => (values: any) => {
+    field.onChange({
+      target: {
+        name,
+        value: formatValue ? formatValue(values.value) : values.value,
+      },
+    });
+  };
 
   return (
-    <Controller
+    <TextInput
       name={name}
-      control={form.control}
-      render={({ field }) => {
-        const onValueChange = (values: any) => {
-          field.onChange({
-            target: {
-              name: props.name,
-              value: formatValue ? formatValue(values.value) : values.value,
-            },
-          });
-        };
-
-        return (
-          <TextField
-            classes={classes}
-            label={localizedLabel}
-            placeholder={localizedPlaceholder}
-            helperText={localizedHelperText}
-            error={Boolean(error)}
-            variant={variant}
-            fullWidth
-            onBlur={field.onBlur}
-            InputLabelProps={{
-              ...InputLabelProps,
-              shrink: true,
-            }}
-            InputProps={{
-              ...InputProps,
-              readOnly,
-              inputComponent: MaskedInputWrapper as any,
-              inputProps: {
-                onValueChange,
-                ...rest,
-              },
-            }}
-          />
-        );
+      onChange={() => {}}
+      onInputPropsValueChange={onValueChange}
+      InputProps={{
+        ...InputProps,
+        readOnly,
+        inputComponent: MaskedInputWrapper as any,
+        inputProps: {
+          ...rest,
+        },
       }}
     />
   );
