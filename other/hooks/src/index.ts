ecc0c07df6863768248e11671042e22be5f94041
Karafizi Arthur <karafizi.artur@gmail.com>
2022-06-19 17:31:17 +0800

    fix: react-hooks by implementing use-interval hook

--- other/hooks/src/index.ts
+++ other/hooks/src/index.ts
@@ -2,3 +2,4 @@ export * from "./use-api-call";
 export * from "./use-collection";
 export * from "./use-did-mount-effect";
 export * from "./use-deep-compare-effect";
+export * from "./use-interval";
