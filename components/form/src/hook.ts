5a69ffecf77886e13cb8b893cba0c3af9fbe1a77
Karafizi Arthur <karafizi.artur@gmail.com>
2022-06-20 08:36:04 +0800

    fix: useYupValidationResolver hook

--- components/form/src/hook.ts
+++ components/form/src/hook.ts
@@ -1,34 +1,31 @@
-import { useCallback } from "react";
 import { Resolver } from "react-hook-form";
 import * as Yup from "yup";
 
-export const useYupValidationResolver = (validate: (data: any) => Promise<any>): Resolver =>
-  useCallback(
-    async (data: any) => {
-      try {
-        const values = await validate(data);
+export const useYupValidationResolver =
+  (validate: (data: any) => Promise<any>): Resolver =>
+  async (data: any) => {
+    try {
+      const values = await validate(data);
 
-        return {
-          values,
-          errors: {},
-        };
-      } catch (errors) {
-        return {
-          values: {},
-          errors: (errors?.inner || []).reduce(
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-            (allErrors: Yup.ValidationError | any, currentError: Yup.ValidationError) => ({
-              ...allErrors,
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              [currentError.path!]: {
-                type: currentError.type ?? "validation",
-                message: currentError.message,
-              },
-            }),
-            {},
-          ),
-        };
-      }
-    },
-    [validate],
-  );
+      return {
+        values,
+        errors: {},
+      };
+    } catch (errors) {
+      return {
+        values: {},
+        errors: (errors?.inner || []).reduce(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          (allErrors: Yup.ValidationError | any, currentError: Yup.ValidationError) => ({
+            ...allErrors,
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            [currentError.path!]: {
+              type: currentError.type ?? "validation",
+              message: currentError.message,
+            },
+          }),
+          {},
+        ),
+      };
+    }
+  };
