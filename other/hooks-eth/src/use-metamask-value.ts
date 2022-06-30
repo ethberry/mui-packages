934a314e7ad695bc557e1941c59f0f3f96995250
Karafizi Arthur <karafizi.artur@gmail.com>
2022-06-30 14:17:45 +0800

    fix: use-deploy by setting errors to form and refactor; resetting form in use-collection and mui-form

--- other/hooks-eth/src/use-metamask-value.ts
+++ other/hooks-eth/src/use-metamask-value.ts
@@ -15,21 +15,9 @@ export const useMetamaskValue = <T = any>(fn: (...args: Array<any>) => Promise<T
     if (!active) {
       openConnectWalletDialog();
       enqueueSnackbar(formatMessage({ id: "snackbar.walletIsNotConnected" }), { variant: "error" });
-      return;
+      return Promise.reject(new Error("Wallet is not connected"));
     }
 
-    return fn(...args)
-      .then((result: T) => {
-        enqueueSnackbar(formatMessage({ id: "snackbar.success" }), { variant: "success" });
-        return result;
-      })
-      .catch((error: any) => {
-        if (error.code === 4001) {
-          enqueueSnackbar(formatMessage({ id: "snackbar.denied" }), { variant: "warning" });
-        } else {
-          console.error(error);
-          enqueueSnackbar(formatMessage({ id: "snackbar.error" }), { variant: "error" });
-        }
-      });
+    return fn(...args);
   };
 };
