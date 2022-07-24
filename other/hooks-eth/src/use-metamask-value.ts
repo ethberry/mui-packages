8d5f53eea254d89f1fad6008efb825cd1cdab835
Karafizi Arthur <karafizi.artur@gmail.com>
2022-07-24 19:36:05 +0800

    fix: implement error processing from JSON provider

--- other/hooks-eth/src/use-metamask-value.ts
+++ other/hooks-eth/src/use-metamask-value.ts
@@ -1,5 +1,6 @@
 import { useSnackbar } from "notistack";
 import { useIntl } from "react-intl";
+import { utils } from "ethers";
 
 export const useMetamaskValue = <T = any>(fn: (...args: Array<any>) => Promise<T>) => {
   const { enqueueSnackbar } = useSnackbar();
@@ -10,8 +11,10 @@ export const useMetamaskValue = <T = any>(fn: (...args: Array<any>) => Promise<T
       if (e.code === 4001) {
         enqueueSnackbar(formatMessage({ id: "snackbar.denied" }), { variant: "warning" });
         return null;
-      } else if (e.code === "UNPREDICTABLE_GAS_LIMIT") {
-        enqueueSnackbar(formatMessage({ id: "snackbar.error" }), { variant: "error" });
+      } else if (e.error?.data?.data) {
+        const data = e.error?.data?.data as string;
+        const decodedMessage = utils.toUtf8String(`0x${data.substr(138)}`);
+        enqueueSnackbar(decodedMessage || formatMessage({ id: "snackbar.error" }), { variant: "error" });
         return null;
       } else {
         throw e;
