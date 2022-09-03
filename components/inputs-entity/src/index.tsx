6969654e6e7faf86356352536c302b4c6d8590f3
Yury Toporov <gemuniongroup@gmail.com>
2022-09-03 14:34:10 +0800

    feat: input entity reason

--- components/inputs-entity/src/index.tsx
+++ components/inputs-entity/src/index.tsx
@@ -29,7 +29,11 @@ export interface IEntityInputProps {
   getTitle?: (item: any) => string;
   data?: Record<string, any>;
   variant?: "standard" | "filled" | "outlined";
-  onChange?: (event: ChangeEvent<unknown>, options: Array<IAutocompleteOption> | IAutocompleteOption | null) => void;
+  onChange?: (
+    event: ChangeEvent<unknown>,
+    options: Array<IAutocompleteOption> | IAutocompleteOption | null,
+    reason: string,
+  ) => void;
 }
 
 export const EntityInput: FC<IEntityInputProps> = props => {
