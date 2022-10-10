9472181a49df6e605a4dcc356dc094f6a865ede6
Karafizi Arthur <karafizi.artur@gmail.com>
2022-10-10 14:11:21 +0800

    fix: formatting big number value for EthInput; fix useCollection to decode big numbers correctly

--- components/inputs-mask/src/eth/utils.ts
+++ components/inputs-mask/src/eth/utils.ts
@@ -1,15 +1,18 @@
 import { BigNumberish, utils } from "ethers";
+import { format, number } from "mathjs";
 
 export const formatValue =
   (units?: BigNumberish) =>
-  (value: string): string =>
-    value ? utils.parseUnits(value, units).toString() : "0";
+  (value: string): string => {
+    return value ? format(number(utils.parseUnits(value, units).toString()), { notation: "fixed" }) : "0";
+  };
 
 export const normalizeValue =
   (units?: BigNumberish) =>
   (value: string): string => {
     // values passed from query string are parsed to number by custom qs.decoder
-    const normalizedValue = value ? utils.formatUnits(value.toString(), units) : "0";
+    const safeValue = format(number(value), { notation: "fixed" });
+    const normalizedValue = value ? utils.formatUnits(safeValue, units) : "0";
     const [whole, decimals] = normalizedValue.split(".");
 
     return decimals === "0" ? whole : normalizedValue;
