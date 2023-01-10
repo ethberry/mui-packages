dc57a5648677cf7113fbacf550a925703003fa8f
Karafizi Arthur <karafizi.artur@gmail.com>
2023-01-10 10:20:28 +0800

    fix: set direction layout (ltr/rtl); implement emotion engine implicitly

--- providers/theme/src/provider/provider.tsx
+++ providers/theme/src/provider/provider.tsx
@@ -1,12 +1,16 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useEffect, useMemo } from "react";
 import { CssBaseline } from "@mui/material";
 import {
   createTheme,
+  Direction,
   PaletteOptions,
-  StyledEngineProvider,
   ThemeOptions,
   ThemeProvider as MuiThemeProvider,
 } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
 
 import { useSettings } from "@gemunion/provider-settings";
 
@@ -21,21 +25,43 @@ export interface IThemeProviderProps {
 export const ThemeProvider: FC<PropsWithChildren<IThemeProviderProps>> = props => {
   const { darkPalette = dark, lightPalette = light, options = {}, children } = props;
   const settings = useSettings();
+  const direction = settings.getLayoutDirection() as Direction;
 
-  const theme = createTheme({
-    palette: {
-      light: lightPalette,
-      dark: darkPalette,
-    }[settings.getTheme()],
-    ...options,
+  const cacheRtl = createCache({
+    key: "muirtl",
+    prepend: true,
+    stylisPlugins: [prefixer, rtlPlugin],
   });
 
+  const cacheLtr = createCache({
+    key: "muiltr",
+    prepend: true,
+    stylisPlugins: [prefixer],
+  });
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          light: lightPalette,
+          dark: darkPalette,
+        }[settings.getTheme()],
+        direction,
+        ...options,
+      }),
+    [direction, lightPalette, darkPalette, options],
+  );
+
+  useEffect(() => {
+    document.dir = direction;
+  }, [direction]);
+
   return (
-    <MuiThemeProvider theme={theme}>
-      <StyledEngineProvider injectFirst>
+    <CacheProvider value={direction === "rtl" ? cacheRtl : cacheLtr}>
+      <MuiThemeProvider theme={theme}>
         <CssBaseline />
         {children}
-      </StyledEngineProvider>
-    </MuiThemeProvider>
+      </MuiThemeProvider>
+    </CacheProvider>
   );
 };
