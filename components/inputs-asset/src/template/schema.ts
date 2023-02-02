d535d43646ae1b7edb9569da2ef2495b56f9f70b
Karafizi Arthur <karafizi.artur@gmail.com>
2023-02-03 01:29:27 +0800

    fix: update schema for template asset

--- components/inputs-asset/src/template/schema.ts
+++ components/inputs-asset/src/template/schema.ts
@@ -9,11 +9,14 @@ export const templateAssetComponentValidationSchema = Yup.object().shape({
     .required("form.validations.valueMissing")
     .integer("form.validations.badInput")
     .min(1, "form.validations.rangeUnderflow"),
-  templateId: Yup.number()
-    .required("form.validations.valueMissing")
-    .integer("form.validations.badInput")
-    .min(1, "form.validations.rangeUnderflow"),
-  amount: bigNumberValidationSchema,
+  templateId: Yup.number().when("tokenType", {
+    is: (tokenType: TokenType) => tokenType !== TokenType.ERC20,
+    then: Yup.number()
+      .min(1, "form.validations.valueMissing")
+      .integer("form.validations.badInput")
+      .required("form.validations.valueMissing"),
+  }),
+  amount: bigNumberValidationSchema.min(1, "form.validations.rangeUnderflow"),
 });
 
 export const templateAssetValidationSchema = Yup.object().shape({
