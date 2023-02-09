5b6c2b2147205857b8cc8451a578794e4db395cc
Karafizi Arthur <karafizi.artur@gmail.com>
2023-02-09 17:34:59 +0800

    fix: customizing for ProgressOverlay

--- components/page-layout/src/progress-overlay/index.tsx
+++ components/page-layout/src/progress-overlay/index.tsx
@@ -1,35 +1,43 @@
-import { Box } from "@mui/material";
+import { Box, SxProps } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
 
 import { Spinner } from "../spinner";
 
 export interface IProgressOverlayProps {
   isLoading: boolean;
+  wrapperSx?: SxProps;
+  spinnerSx?: SxProps;
 }
 
 export const ProgressOverlay: FC<PropsWithChildren<IProgressOverlayProps>> = props => {
-  const { isLoading, children } = props;
+  const { isLoading, children, wrapperSx = {}, spinnerSx = {} } = props;
 
   return (
     <Box
-      sx={{
-        position: "relative",
-        minHeight: 40,
-      }}
+      sx={[
+        {
+          position: "relative",
+          minHeight: 40,
+        },
+        ...(Array.isArray(wrapperSx) ? wrapperSx : [wrapperSx]),
+      ]}
     >
       {children}
       {isLoading ? (
         <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
+          sx={[
+            {
+              position: "absolute",
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            },
+            ...(Array.isArray(spinnerSx) ? spinnerSx : [spinnerSx]),
+          ]}
         >
           <Spinner />
         </Box>
