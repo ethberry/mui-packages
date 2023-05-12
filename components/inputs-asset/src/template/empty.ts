eb6278ae62d997b7ddb33fc1c7abfce4c460b2fd
Yury Toporov <gemuniongroup@gmail.com>
2023-05-12 21:27:47 +0800

    fix: empty template

--- components/inputs-asset/src/template/empty.ts
+++ components/inputs-asset/src/template/empty.ts
@@ -8,15 +8,35 @@ export function getEmptyTemplate(tokenType = TokenType.ERC20): any {
   return {
     components: [
       {
+        id: 0,
         tokenType,
         contractId: 0,
         contract: {
+          id: 0,
+          title: "",
+          contractType: tokenType,
           decimals: tokenType === TokenType.NATIVE || tokenType === TokenType.ERC20 ? 18 : 0,
           address: constants.AddressZero,
         },
         templateId: 0,
         template: {
-          imgUrl:
+          id: 0,
+          title: "",
+          description: JSON.stringify({
+            blocks: [
+              {
+                key: "ee782",
+                text: "",
+                type: "unstyled",
+                depth: 0,
+                inlineStyleRanges: [],
+                entityRanges: [],
+                data: {},
+              },
+            ],
+            entityMap: {},
+          }),
+          imageUrl:
             "https://firebasestorage.googleapis.com/v0/b/gemunion-firebase.appspot.com/o/DO_NOT_REMOVE_LOGO.png?alt=media&token=85c376a8-33a0-4b6b-9285-2b9022287289",
         },
         amount:
--- components/inputs-asset/src/template/types.ts
+++ components/inputs-asset/src/template/types.ts
@@ -1,15 +1,22 @@
 import type { TokenType } from "@gemunion/types-blockchain";
 
 export interface ITemplateAssetComponent {
+  id: number;
   tokenType: TokenType;
   contractId: number;
   contract: {
+    id: number;
+    title: string;
     address: string;
     decimals: number;
     contractType: TokenType;
   };
   templateId: number;
   template: {
+    id: number;
+    description: string;
+    imageUrl: string;
+    title: string;
     imgUrl: string;
   };
   amount: string;
