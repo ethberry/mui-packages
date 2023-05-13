31702068b4ee3e7edcb2c8752461e3e0767396d1
TrejGun <trejgun@gmail.com>
2023-05-13 08:04:20 +0800

    fix: types

--- providers/picker/src/provider/provider.tsx
+++ providers/picker/src/provider/provider.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from "react";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider, LocalizationProviderProps } from "@mui/x-date-pickers/LocalizationProvider";
 
-export const PickerProvider: FC<PropsWithChildren<LocalizationProviderProps<any>>> = props => {
+export const PickerProvider: FC<PropsWithChildren<LocalizationProviderProps<Date, any>>> = props => {
   const { children, ...rest } = props;
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} {...rest}>
