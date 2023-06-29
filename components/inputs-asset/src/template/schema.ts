ec1a357391c0e3b6cbabc7d1320617c74545bed3
Karafizi Arthur <karafizi.artur@gmail.com>
2023-06-29 12:33:24 +0800

    fix: split token and template assets validation schema

--- components/inputs-asset/src/template/schema.ts
+++ components/inputs-asset/src/template/schema.ts
@@ -3,26 +3,36 @@ import { array, mixed, number, object } from "yup";
 import { bigNumberValidationSchema } from "@gemunion/yup-rules-eth";
 import { TokenType } from "@gemunion/types-blockchain";
 
+export const templateAssetTokenTypeValidationSchema = mixed<TokenType>()
+  .oneOf(Object.values(TokenType))
+  .required("form.validations.valueMissing");
+
+export const templateAssetContractIdValidationSchema = number()
+  .required("form.validations.valueMissing")
+  .integer("form.validations.badInput")
+  .min(1, "form.validations.rangeUnderflow");
+
+export const templateAssetTemplateIdValidationSchema = number().when("tokenType", {
+  is: (tokenType: TokenType) => tokenType !== TokenType.ERC20 && tokenType !== TokenType.NATIVE,
+  then: schema =>
+    schema
+      .min(1, "form.validations.valueMissing")
+      .integer("form.validations.badInput")
+      .required("form.validations.valueMissing"),
+});
+
+export const templateAssetAmountValidationSchema = bigNumberValidationSchema.when("tokenType", {
+  is: (tokenType: TokenType) => tokenType !== TokenType.ERC721 && tokenType !== TokenType.ERC998,
+  then: () =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    bigNumberValidationSchema.min(1, "form.validations.rangeUnderflow").required("form.validations.valueMissing"),
+});
+
 export const templateAssetComponentValidationSchema = object().shape({
-  tokenType: mixed<TokenType>().oneOf(Object.values(TokenType)).required("form.validations.valueMissing"),
-  contractId: number()
-    .required("form.validations.valueMissing")
-    .integer("form.validations.badInput")
-    .min(1, "form.validations.rangeUnderflow"),
-  templateId: number().when("tokenType", {
-    is: (tokenType: TokenType) => tokenType !== TokenType.ERC20 && tokenType !== TokenType.NATIVE,
-    then: schema =>
-      schema
-        .min(1, "form.validations.valueMissing")
-        .integer("form.validations.badInput")
-        .required("form.validations.valueMissing"),
-  }),
-  amount: bigNumberValidationSchema.when("tokenType", {
-    is: (tokenType: TokenType) => tokenType !== TokenType.ERC721 && tokenType !== TokenType.ERC998,
-    then: () =>
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      bigNumberValidationSchema.min(1, "form.validations.rangeUnderflow").required("form.validations.valueMissing"),
-  }),
+  tokenType: templateAssetTokenTypeValidationSchema,
+  contractId: templateAssetContractIdValidationSchema,
+  templateId: templateAssetTemplateIdValidationSchema,
+  amount: templateAssetAmountValidationSchema,
 });
 
 export const templateAssetValidationSchema = object().shape({
--- components/inputs-asset/src/token/schema.ts
+++ components/inputs-asset/src/token/schema.ts
@@ -3,28 +3,38 @@ import { array, mixed, number, object } from "yup";
 import { bigNumberValidationSchema } from "@gemunion/yup-rules-eth";
 import { TokenType } from "@gemunion/types-blockchain";
 
+export const tokenAssetTokenTypeValidationSchema = mixed<TokenType>()
+  .oneOf(Object.values(TokenType))
+  .required("form.validations.valueMissing");
+
+export const tokenAssetContractIdValidationSchema = number()
+  .required("form.validations.valueMissing")
+  .integer("form.validations.badInput")
+  .min(1, "form.validations.rangeUnderflow");
+
+export const tokenAssetTokenValidationSchema = object().when("tokenType", {
+  is: (tokenType: TokenType) => tokenType !== TokenType.ERC20 && tokenType !== TokenType.NATIVE,
+  then: schema =>
+    schema.shape({
+      tokenId: number()
+        .min(1, "form.validations.valueMissing")
+        .integer("form.validations.badInput")
+        .required("form.validations.valueMissing"),
+    }),
+});
+
+export const tokenAssetAmountValidationSchema = bigNumberValidationSchema.when("tokenType", {
+  is: (tokenType: TokenType) => tokenType !== TokenType.ERC721 && tokenType !== TokenType.ERC998,
+  then: () =>
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    bigNumberValidationSchema.min(1, "form.validations.rangeUnderflow").required("form.validations.valueMissing"),
+});
+
 export const tokenAssetComponentValidationSchema = object().shape({
-  tokenType: mixed<TokenType>().oneOf(Object.values(TokenType)).required("form.validations.valueMissing"),
-  contractId: number()
-    .required("form.validations.valueMissing")
-    .integer("form.validations.badInput")
-    .min(1, "form.validations.rangeUnderflow"),
-  token: object().when("tokenType", {
-    is: (tokenType: TokenType) => tokenType !== TokenType.ERC20 && tokenType !== TokenType.NATIVE,
-    then: schema =>
-      schema.shape({
-        tokenId: number()
-          .min(1, "form.validations.valueMissing")
-          .integer("form.validations.badInput")
-          .required("form.validations.valueMissing"),
-      }),
-  }),
-  amount: bigNumberValidationSchema.when("tokenType", {
-    is: (tokenType: TokenType) => tokenType !== TokenType.ERC721 && tokenType !== TokenType.ERC998,
-    then: () =>
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      bigNumberValidationSchema.min(1, "form.validations.rangeUnderflow").required("form.validations.valueMissing"),
-  }),
+  tokenType: tokenAssetTokenTypeValidationSchema,
+  contractId: tokenAssetContractIdValidationSchema,
+  token: tokenAssetTokenValidationSchema,
+  amount: tokenAssetAmountValidationSchema,
 });
 
 export const tokenAssetValidationSchema = object().shape({
