a81a559e2e34decde5d05050b38a254419c382db
TrejGun <trejgun@gmail.com>
2023-07-18 17:07:03 +0800

    chore: update gh actions

--- scripts/obfuscate.sh
+++ scripts/obfuscate.sh
@@ -4,5 +4,7 @@ echo -e "\033[34mObfuscating...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
-javascript-obfuscator ./ --config jsobsrc.json --exclude 'node_modules/, .storybook/, forks/s3-uploader/src/'
+javascript-obfuscator . --config jsobsrc.json --exclude 'node_modules/, .storybook/'
 
+find . -name '*obfuscated.js' -exec bash -c 'mv "$1" "${1/-obfuscated/}"' _ {} \;
+find . -name '*obfuscated.*.js' -exec bash -c 'mv -v "$1" "${1/-obfuscated/}"' _ {} \;
