bdd83f1cb7b12b76f1f1d30b8c327cd54cb17428
TrejGun <trejgun@gmail.com>
2023-07-31 10:32:14 +0800

    fix: add details to rate plans

--- other/license-pages/src/rate-plan/index.tsx
+++ other/license-pages/src/rate-plan/index.tsx
@@ -1,9 +1,8 @@
 import { FC } from "react";
 import { Grid } from "@mui/material";
-import { useIntl } from "react-intl";
+import { FormattedMessage } from "react-intl";
 import { Clear, Done } from "@mui/icons-material";
 
-import { useRatePlans } from "./hook";
 import {
   Divider,
   FeatureIconExcluded,
@@ -21,20 +20,78 @@ import {
   TitleTypography,
 } from "./styled";
 
-export const RatePlansSelection: FC = () => {
-  const { formatMessage } = useIntl();
-  const ratePlans = useRatePlans();
+const ratePlans = [
+  {
+    title: "bronze",
+    price: "2000",
+    features: [
+      { name: "erc20", included: true },
+      { name: "erc721", included: true },
+      { name: "erc1155", included: true },
+      { name: "vesting", included: true },
+      { name: "disperse", included: true },
+      { name: "claim", included: true },
+      { name: "grade", included: true },
+      { name: "craft", included: true },
+      { name: "mystery", included: false },
+      // { name: "breeding", included: false },
+      // { name: "drop", included: false },
+      // { name: "rent", included: false },
+      // { name: "wrapper", included: false },
+      // { name: "collection", included: false },
+      // { name: "voucher", included: false },
+      // { name: "merge", included: false },
+    ],
+  },
+  {
+    title: "silver",
+    price: "5000",
+    features: [
+      { name: "erc20", included: true },
+      { name: "erc721", included: true },
+      { name: "erc1155", included: true },
+      { name: "vesting", included: true },
+      { name: "disperse", included: true },
+      { name: "claim", included: true },
+      { name: "grade", included: true },
+      { name: "craft", included: true },
+      { name: "mystery", included: true },
+    ],
+  },
+  {
+    title: "gold",
+    price: "9000",
+    features: [
+      { name: "erc20", included: true },
+      { name: "erc721", included: true },
+      { name: "erc1155", included: true },
+      { name: "vesting", included: true },
+      { name: "disperse", included: true },
+      { name: "claim", included: true },
+      { name: "grade", included: true },
+      { name: "craft", included: true },
+      { name: "mystery", included: true },
+    ],
+  },
+];
 
+export const RatePlansSelection: FC = () => {
   return (
     <Grid container justifyContent="center" spacing={2}>
       {ratePlans.map((plan, index) => (
         <StyledCardWrapper item xs={12} sm={6} md={4} key={index}>
           <StyledCard>
             <StyledCardContent>
-              <TitleTypography variant="h6">{plan.title}</TitleTypography>
+              <TitleTypography variant="h6">
+                <FormattedMessage id={plan.title} />
+                <FormattedMessage id={`pages.ratePlan.${plan.title}`} />
+              </TitleTypography>
               <PriceWrapper>
-                <PriceTypography variant="h3">{plan.price}</PriceTypography>
-                <MonthTypography> / {formatMessage({ id: "pages.ratePlan.month" })}</MonthTypography>
+                <PriceTypography variant="h3">${plan.price}</PriceTypography>
+                <MonthTypography>
+                  {" / "}
+                  <FormattedMessage id="pages.ratePlan.month" />
+                </MonthTypography>
               </PriceWrapper>
               <Divider />
               <FeaturesWrapper>
@@ -47,7 +104,9 @@ export const RatePlansSelection: FC = () => {
                         <FeatureIconExcluded component={Clear} />
                       )}
                     </FeatureIconWrapper>
-                    <FeatureTypography included={feature.included}>{feature.name}</FeatureTypography>
+                    <FeatureTypography included={feature.included}>
+                      <FormattedMessage id={`pages.ratePlan.${plan.title}.${feature.name}`} />
+                    </FeatureTypography>
                   </FeatureWrapper>
                 ))}
               </FeaturesWrapper>
@@ -59,7 +118,9 @@ export const RatePlansSelection: FC = () => {
       <StyledCardWrapper item xs={12}>
         <StyledCard>
           <StyledCardContent>
-            <TitleTypography variant="h6">{formatMessage({ id: "pages.ratePlan.alwaysIncluded" })}</TitleTypography>
+            <TitleTypography variant="h6">
+              <FormattedMessage id="pages.ratePlan.onDemand.title" />
+            </TitleTypography>
             <Divider />
             <Grid container spacing={{ sm: 0, md: 4 }} justifyContent="center">
               <Grid item xs={12} sm={6} md={4} justifyContent="center">
@@ -67,7 +128,9 @@ export const RatePlansSelection: FC = () => {
                   <FeatureIconWrapper>
                     <FeatureIconIncluded component={Done} />
                   </FeatureIconWrapper>
-                  <FeatureTypography included>Transaction history</FeatureTypography>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.onDemand.waitList" />
+                  </FeatureTypography>
                 </FeatureWrapper>
               </Grid>
               <Grid item xs={12} sm={6} md={4} justifyContent="center">
@@ -75,7 +138,9 @@ export const RatePlansSelection: FC = () => {
                   <FeatureIconWrapper>
                     <FeatureIconIncluded component={Done} />
                   </FeatureIconWrapper>
-                  <FeatureTypography included>OpenSea integration</FeatureTypography>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.onDemand.raffle" />
+                  </FeatureTypography>
                 </FeatureWrapper>
               </Grid>
               <Grid item xs={12} sm={6} md={4} justifyContent="center">
@@ -83,7 +148,52 @@ export const RatePlansSelection: FC = () => {
                   <FeatureIconWrapper>
                     <FeatureIconIncluded component={Done} />
                   </FeatureIconWrapper>
-                  <FeatureTypography included>Marketing insights</FeatureTypography>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.onDemand.staking" />
+                  </FeatureTypography>
+                </FeatureWrapper>
+              </Grid>
+            </Grid>
+          </StyledCardContent>
+        </StyledCard>
+      </StyledCardWrapper>
+
+      <StyledCardWrapper item xs={12}>
+        <StyledCard>
+          <StyledCardContent>
+            <TitleTypography variant="h6">
+              <FormattedMessage id="pages.ratePlan.alwaysIncluded.title" />
+            </TitleTypography>
+            <Divider />
+            <Grid container spacing={{ sm: 0, md: 4 }} justifyContent="center">
+              <Grid item xs={12} sm={6} md={4} justifyContent="center">
+                <FeatureWrapper sx={{ justifyContent: "center" }}>
+                  <FeatureIconWrapper>
+                    <FeatureIconIncluded component={Done} />
+                  </FeatureIconWrapper>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.alwaysIncluded.history" />
+                  </FeatureTypography>
+                </FeatureWrapper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} justifyContent="center">
+                <FeatureWrapper sx={{ justifyContent: "center" }}>
+                  <FeatureIconWrapper>
+                    <FeatureIconIncluded component={Done} />
+                  </FeatureIconWrapper>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.alwaysIncluded.openSea" />
+                  </FeatureTypography>
+                </FeatureWrapper>
+              </Grid>
+              <Grid item xs={12} sm={6} md={4} justifyContent="center">
+                <FeatureWrapper sx={{ justifyContent: "center" }}>
+                  <FeatureIconWrapper>
+                    <FeatureIconIncluded component={Done} />
+                  </FeatureIconWrapper>
+                  <FeatureTypography included>
+                    <FormattedMessage id="pages.ratePlan.alwaysIncluded.insights" />
+                  </FeatureTypography>
                 </FeatureWrapper>
               </Grid>
             </Grid>
