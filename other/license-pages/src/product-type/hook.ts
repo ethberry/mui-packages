bbe7ec1a9496adf487f6812386a281e08ba78fdd
Karafizi Arthur <karafizi.artur@gmail.com>
2023-08-04 20:43:15 +0800

    fix: obfuscate GEMUNION_URL for product types

--- other/license-pages/src/product-type/hook.ts
+++ other/license-pages/src/product-type/hook.ts
@@ -4,6 +4,10 @@ export interface IUseProductTypesProps {
   internal: boolean;
 }
 
+/* javascript-obfuscator:disable */
+const gemunionUrl = process.env.GEMUNION_URL;
+/* javascript-obfuscator:enable */
+
 export const useProductTypes = (props: IUseProductTypesProps) => {
   const { internal } = props;
   const { formatMessage } = useIntl();
@@ -12,19 +16,19 @@ export const useProductTypes = (props: IUseProductTypesProps) => {
     {
       title: formatMessage({ id: "components.productTypes.saas.title" }),
       text: formatMessage({ id: "components.productTypes.saas.text" }),
-      link: internal ? "/calculator-saas" : `${process.env.GEMUNION_URL}/calculator-saas`,
+      link: internal ? "/calculator-saas" : `${gemunionUrl}/calculator-saas`,
       linkTitle: formatMessage({ id: "components.productTypes.saas.linkTitle" }),
     },
     {
       title: formatMessage({ id: "components.productTypes.selfHosted.title" }),
       text: formatMessage({ id: "components.productTypes.selfHosted.text" }),
-      link: internal ? "/calculator" : `${process.env.GEMUNION_URL}/calculator`,
+      link: internal ? "/calculator" : `${gemunionUrl}/calculator`,
       linkTitle: formatMessage({ id: "components.productTypes.selfHosted.linkTitle" }),
     },
     {
       title: formatMessage({ id: "components.productTypes.enterprise.title" }),
       text: formatMessage({ id: "components.productTypes.enterprise.text" }),
-      link: internal ? "/contact-enterprise" : `${process.env.GEMUNION_URL}/contact-enterprise`,
+      link: internal ? "/contact-enterprise" : `${gemunionUrl}/contact-enterprise`,
       linkTitle: formatMessage({ id: "components.productTypes.enterprise.linkTitle" }),
     },
   ];
