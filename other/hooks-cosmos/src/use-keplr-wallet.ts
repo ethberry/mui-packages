456450cf4f7d9ebaae3048da6123172534a3c0fb
TrejGun <trejgun@gmail.com>
2023-09-15 14:28:23 +0800

    fix: simplify useKepler

--- other/hooks-cosmos/src/use-keplr-wallet.ts
+++ other/hooks-cosmos/src/use-keplr-wallet.ts
@@ -2,9 +2,7 @@ import { enqueueSnackbar } from "notistack";
 import { useIntl } from "react-intl";
 import { Keplr, OfflineAminoSigner, OfflineDirectSigner } from "@keplr-wallet/types";
 
-import { downForMaintenance } from "@gemunion/license-messages";
 import { useCosmos } from "@gemunion/provider-cosmos";
-import { useLicense } from "@gemunion/provider-license";
 
 import { IHandlerOptionsParams } from "./interfaces";
 
@@ -12,39 +10,28 @@ export const useKeplrWallet = <T = any>(
   fn: (keplr: Keplr, offlineSigner: OfflineAminoSigner & OfflineDirectSigner, ...args: Array<any>) => Promise<T>,
   options: IHandlerOptionsParams = {},
 ) => {
-  const license = useLicense();
   const { setAccount, setIsKeplrConnected } = useCosmos();
 
   const { formatMessage } = useIntl();
   const { success = true, error = true } = options;
 
   return async (...args: Array<any>): Promise<T> => {
-    if (!license.isValid()) {
-      return Promise.reject(downForMaintenance()).catch((e: string) => {
-        enqueueSnackbar(e, { variant: "error" });
-        return null as unknown as T;
-      });
+    const { keplr, getOfflineSigner, open } = window;
+
+    const chainId = "haqq_11235-1";
+
+    if (!keplr || !getOfflineSigner) {
+      open("https://www.keplr.app/download", "_blank", "noopener noreferrer");
+      enqueueSnackbar("Keplr not found", { variant: "error" });
+      return null as unknown as T;
     }
 
-    let keplr;
-    let offlineSigner;
-
-    if (window.keplr && window.getOfflineSigner) {
-      const chainId = "haqq_11235-1";
-      keplr = window.keplr;
-      offlineSigner = window.getOfflineSigner(chainId);
-      await window.keplr.enable(chainId);
-      const accounts = await offlineSigner.getAccounts();
-      const account = accounts[0].address;
-      setAccount(account);
-      setIsKeplrConnected(true);
-    } else {
-      window.open("https://www.keplr.app/download", "_blank", "noopener noreferrer");
-      return Promise.reject(new Error("Keplr not found")).catch((e: string) => {
-        enqueueSnackbar(e, { variant: "error" });
-        return null as unknown as T;
-      });
-    }
+    const offlineSigner = getOfflineSigner(chainId);
+    await keplr.enable(chainId);
+    const accounts = await offlineSigner.getAccounts();
+    const account = accounts[0].address;
+    setAccount(account);
+    setIsKeplrConnected(true);
 
     return fn(keplr, offlineSigner, ...args)
       .then((result: any) => {
--- other/hooks-cosmos/src/use-keplr.ts
+++ other/hooks-cosmos/src/use-keplr.ts
@@ -1,9 +1,7 @@
 import { enqueueSnackbar } from "notistack";
 import { useIntl } from "react-intl";
 
-import { downForMaintenance } from "@gemunion/license-messages";
-import { useLicense } from "@gemunion/provider-license";
-import { useCosmos, chainInfoByChainId } from "@gemunion/provider-cosmos";
+import { chainInfoByChainId, useCosmos } from "@gemunion/provider-cosmos";
 
 import { ICosmosParams, IHandlerOptionsParams } from "./interfaces";
 
@@ -11,38 +9,23 @@ export const useKeplr = <T = any>(
   fn: (cosmosParams: ICosmosParams, ...args: Array<any>) => Promise<T>,
   options: IHandlerOptionsParams = {},
 ) => {
-  const license = useLicense();
-
   const { formatMessage } = useIntl();
   const { enabledChains } = useCosmos();
   const { success = true, error = true } = options;
 
   return async (...args: Array<any>): Promise<T> => {
-    if (!license.isValid()) {
-      return Promise.reject(downForMaintenance()).catch((e: string) => {
-        enqueueSnackbar(e, { variant: "error" });
-        return null as unknown as T;
-      });
+    const { keplr, getOfflineSigner, open } = window;
+
+    if (!keplr || !getOfflineSigner) {
+      open("https://www.keplr.app/download", "_blank", "noopener noreferrer");
+      enqueueSnackbar("Keplr not found", { variant: "error" });
+      return null as unknown as T;
     }
 
-    let keplr;
-    let getOfflineSigner;
-
-    if (window.keplr && window.getOfflineSigner) {
-      keplr = window.keplr;
-      getOfflineSigner = window.getOfflineSigner;
-
-      for (const chainId of enabledChains) {
-        await window.keplr.experimentalSuggestChain(chainInfoByChainId[chainId]);
-      }
-      await window.keplr.enable(enabledChains);
-    } else {
-      window.open("https://www.keplr.app/download", "_blank", "noopener noreferrer");
-      return Promise.reject(new Error("Keplr not found")).catch((e: string) => {
-        enqueueSnackbar(e, { variant: "error" });
-        return null as unknown as T;
-      });
+    for (const chainId of enabledChains) {
+      await keplr.experimentalSuggestChain(chainInfoByChainId[chainId]);
     }
+    await keplr.enable(enabledChains);
 
     return fn({ keplr, getOfflineSigner }, ...args)
       .then((result: any) => {
