084ef6a4dc9bf1da1977ddfd71302bdfc62ebe8a
Yury Toporov <gemuniongroup@gmail.com>
2024-01-17 18:25:36 +0800

    fix: particle icons

--- providers/wallet/src/dialog/buttons/utils.tsx
+++ providers/wallet/src/dialog/buttons/utils.tsx
@@ -2,13 +2,9 @@ import { Facebook } from "@mui/icons-material";
 import { AuthType } from "@particle-network/auth";
 
 import { GoogleIcon } from "../social-icons";
-import { ParticleIcon } from "../wallet-icons";
 
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-export const getParticleButtonIcon = (type: AuthType | "particle"): any => {
+export const getParticleButtonIcon = (type: AuthType): any => {
   switch (type) {
-    case "particle":
-      return <ParticleIcon />;
     case "facebook":
       return <Facebook sx={{ color: "#4267B2" }} />;
     case "google":
