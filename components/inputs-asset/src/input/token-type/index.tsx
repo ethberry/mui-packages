5865dffb8d2859a5d1462c830a931ab7164815d2
TrejGun <trejgun@gmail.com>
2024-03-02 20:30:59 +0800

    fix: mui types

--- components/inputs-asset/src/input/token-type/index.tsx
+++ components/inputs-asset/src/input/token-type/index.tsx
@@ -2,18 +2,25 @@ import { FC } from "react";
 
 import { SelectInput } from "@gemunion/mui-inputs-core";
 import { TokenType } from "@gemunion/types-blockchain";
+import { SelectProps } from "@mui/material";
 
-export interface ITokenTypeInputProps {
+export type ITokenTypeInputProps = {
   prefix: string;
   name?: string;
   readOnly?: boolean;
   disabledOptions?: Array<TokenType>;
-}
+} & SelectProps;
 
 export const TokenTypeInput: FC<ITokenTypeInputProps> = props => {
-  const { prefix, name = "tokenType", disabledOptions = [], readOnly } = props;
+  const { prefix, name = "tokenType", variant = "standard", disabledOptions = [], readOnly } = props;
 
   return (
-    <SelectInput name={`${prefix}.${name}`} options={TokenType} disabledOptions={disabledOptions} readOnly={readOnly} />
+    <SelectInput
+      name={`${prefix}.${name}`}
+      options={TokenType}
+      disabledOptions={disabledOptions}
+      variant={variant}
+      readOnly={readOnly}
+    />
   );
 };
