e265d8c026b87774a72306d7873cf4b5e5507cf3
TrejGun <trejgun@gmail.com>
2024-06-11 10:32:24 +0800

    fix: remove custom buttons, part 2

--- providers/wallet/src/index.ts
+++ providers/wallet/src/index.ts
@@ -3,7 +3,6 @@ export * from "./chain-icons";
 export * from "./connectors";
 export * from "./dialog";
 export * from "./dialog/buttons";
-export * from "./dialog/social-icons";
 export * from "./dialog/wallet-icons";
 export * from "./hooks";
 export * from "./provider";
