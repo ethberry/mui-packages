9dc6b93177cfde658d45e532bf465e409ed9a4b7
TrejGun <trejgun@gmail.com>
2024-07-24 11:40:47 +0100

    chore: move cosmos related packages to mui-packages-cosmos

--- other/hooks-cosmos/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./use-address-converter";
-export * from "./use-keplr";
-export * from "./use-keplr-wallet";
--- providers/cosmos/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./button";
-export * from "./dialog";
-export * from "./dialog/wallet-icons";
-export * from "./hooks";
-export * from "./interfaces";
-export * from "./provider";
