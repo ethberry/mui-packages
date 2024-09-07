b0815ad613a5e4aacd20bd9c4e98ca6f597dbdee
TrejGun <trejgun@gmail.com>
2024-09-07 15:02:12 +0800

    fix: version, test and types

--- components/page-layout/src/error-boundary/styled.tsx
+++ components/page-layout/src/error-boundary/styled.tsx
@@ -13,25 +13,25 @@ export const StyledError = styled(Box)(({ theme }) => ({
   minHeight: `calc(100vh - ${theme.spacing(16)})`,
   width: "100%",
   marginTop: nodeEnv !== "production" ? theme.spacing(4) : 0,
-}));
+})) as typeof Box;
 
 export const StyledAlert = styled(Alert)({
   width: "100%",
   "& .MuiAlert-action": {
     paddingTop: 0,
   },
-});
+}) as typeof Alert;
 
 export const StyledPreWrapper = styled(Box)({
   width: "100%",
-});
+}) as typeof Box;
 
 export const StyledPreTop = styled(Box)({
   marginBottom: 0,
   textWrap: "balance",
-});
+}) as typeof Box;
 
 export const StyledPreBottom = styled(Box)({
   marginTop: 0,
   textWrap: "balance",
-});
+}) as typeof Box;
